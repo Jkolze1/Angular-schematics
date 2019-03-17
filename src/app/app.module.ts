7c6095169dc09c0ddf8d90c98ae1c77ae60f6543
Jacqueline Kolze <jkolze1@stumail.jccc.edu>
2019-03-16 20:15:58 -0500

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TodoComponent } from './todo/todo.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  imports: [     
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    DragDropModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
+  declarations: [ 
+    AppComponent,
+    NavigationComponent,
+    DashboardComponent,
+    TodoComponent,
+    TableComponent
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
